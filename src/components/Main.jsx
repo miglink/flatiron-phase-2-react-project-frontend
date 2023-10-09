95ad6729b6fb1ecbf9e602c0891a72ec658472a6
Michelle Glinkowski <miglinkowski@aol.com>
2023-10-08 19:44:11 -0500

    feat: add Main components

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+const Main = () => {
+    return (
+        <>
+            <main>
+                <h2>The main content of our application</h2>
+                <p>
+                    some text
+                </p>
+            </main>
+        </>
+    )
+}
+
+export default Main;
