0c84455e188e86a7efc2ebc3b86a21432f2b515f
Michelle Glinkowski <miglinkowski@aol.com>
2023-10-15 19:01:31 -0500

    added character info+images to character component

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 
-const Character = () => {
+const Character = ({character}) => {
+    const {name, films, shortfilms, tvShows, parkAttractions, allies, enemies, imageUrl} = character
     return (
-        <div>Character</div>
+        <div className="character" >
+            <img src={imageUrl} alt={`Character Portrait of ${name}`} />
+            <h2>{name}</h2>
+            <p>{films}</p>
+            <p>{shortfilms}</p>
+            <p>{tvShows}</p>
+            <p>{parkAttractions}</p>
+            <p>{allies}</p>
+            <p>{enemies}</p>
+        </div>
     )
 }
 
